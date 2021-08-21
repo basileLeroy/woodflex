56e70d5c48417a36f85079fba61ee0fa4cb963db
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-08-21 14:00:28 +0200

    Adding 404 template to project

--- client/src/pages/error/Error.js
+++ client/src/pages/error/Error.js
@@ -1,20 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Error() {
     return (
         <>
-            <div className="flex flex-row mx-auto">
-                <div className="m-auto my-8 lg:my-4 relative bg-blue-100 lg:w-3/5 max-w-4xl w-nine text-gray-600 text-center font-extrabold text-4xl rounded-2xl">
-                    <div className="Error m-8">
-                        <h1 className="absolute hidden lg:block top-8 left-96">Woops! ...</h1>
-                        <p className="absolute hidden lg:block top-40 text-2xl left-96">Looks like our dwellers fell asleep again.. This page is not available</p>
-                        <p className="absolute hidden lg:block bottom-4 text-lg right-4">Error Code: <span className="text-red-600"> 404</span> </p>
-
-                        <h1 className="lg:hidden">Woops! ...</h1>
-                        <p className="lg:hidden text-2xl">Looks like our dwellers fell asleep again.. This page is not available</p>
-                        <p className="lg:hidden text-lg">Error Code: <span className="text-red-600"> 404</span> </p>
+            <div className="gradient text-white min-h-screen flex items-center">
+                <div className="container mx-auto p-4 flex flex-wrap items-center">
+                    <div className="w-full md:w-5/12 text-center p-4">
+                        <img src="https://themichailov.com/img/not-found.svg" alt="Not Found"/>
+                    </div>
+                    <div className="w-full md:w-7/12 text-center md:text-left p-4">
+                        <div className="text-6xl font-medium">404</div>
+                        <div className="text-xl md:text-3xl font-medium mb-4">
+                            Oops. This page has gone missing.
+                        </div>
+                        <div className="text-lg mb-8">
+                            You may have mistyped the address or the page may have moved.
+                        </div>
+                        <Link to="/" className="border border-white rounded p-4">Go Home</Link>
                     </div>
                 </div>
+
             </div>
         </>
     )
