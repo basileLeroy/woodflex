a86e3160f826b1b70943649a3e52ed31b0025553
Basile <basile.leroy.jnm@hotmail.com>
2021-10-10 13:12:59 +0200

    Adding title to Iframe in contacts

--- client/src/pages/contact/ContactInfo.js
+++ client/src/pages/contact/ContactInfo.js
@@ -24,6 +24,7 @@ const ContactInfo = () => {
                                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2507.940291882634!2d3.7212237156393058!3d51.05419027956328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c37146f023fafb%3A0x155761ea40a108bf!2sKlein%20Turkije%202a%2C%209000%20Gent!5e0!3m2!1sen!2sbe!4v1632043562937!5m2!1sen!2sbe"
                                     width="300"
                                     height="200"
+                                    title="Location"
                                     style={{ border: 0 }}
                                     allowFullScreen=""
                                     loading="lazy">
